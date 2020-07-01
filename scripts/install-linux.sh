bcaefaf9a412531d8b260573b4f81b2671e9efcf
Jordan Mance <mancej@snagajob.com>
2020-06-30 20:42:38 -0400

    Tweaking install script

--- scripts/install-linux.sh
+++ scripts/install-linux.sh
@@ -5,7 +5,7 @@
 install_dir="${HOME}/.figgy/installations/first"
 
 # Linux Example (change download URL for MacOs)
-echo "Installing figgy in directory: ${install_dir}"
+echo "Instaling figgy in directory: ${install_dir}"
 mkdir -p ${install_dir}
 
 # Download Figgy
@@ -13,6 +13,15 @@ echo "Downloading..."
 cd ${install_dir} && curl -s https://www.figgy.dev/releases/cli/latest/linux/figgy.zip > figgy.zip
 
 # Unzip Figgy
-echo "Unzipping..."
+echo "Unzipping (silently to prevent spam)..."
 unzip figgy.zip > /dev/null
+
+# Symlink figgy
+echo "Creating symlink at /usr/local/bin/figgy"
+sudo ln -snf ${install_dir}/figgy/figgy /usr/local/bin/figgy
+
+
+# Test Figgy
+echo "Testing install with:  'figgy --version'"
+figgy --version
 
