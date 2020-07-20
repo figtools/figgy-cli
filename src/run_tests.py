bf3ee2c3ed40782fd33c26813403fa2ac37c66cf
Jordan Mance <mancej@snagajob.com>
2020-07-20 00:23:07 -0400

    Enabling all tests

--- src/run_tests.py
+++ src/run_tests.py
@@ -127,16 +127,16 @@ def main():
 
 def dev_tests(profile=None, key_down_to_shared=4):
     extra_args = f"--profile {profile}" if profile else ""
-    # run_test("Dev Put", DevPut(extra_args=extra_args))
-    # run_test("Dev Get", DevGet(extra_args=extra_args))
-    # run_test("Dev Delete", DevDelete(extra_args=extra_args))
-    # run_test("Dev Dump", DevDump(extra_args=extra_args))
-    #
-    # if not profile:
-    #     run_test("Dev Promote", DevPromote(extra_args=extra_args))
-    #
-    # # run_test("Dev List", DevList(extra_args=extra_args))
-    # run_test("Dev Audit", DevAudit(extra_args=extra_args))
+    run_test("Dev Put", DevPut(extra_args=extra_args))
+    run_test("Dev Get", DevGet(extra_args=extra_args))
+    run_test("Dev Delete", DevDelete(extra_args=extra_args))
+    run_test("Dev Dump", DevDump(extra_args=extra_args))
+
+    if not profile:
+        run_test("Dev Promote", DevPromote(extra_args=extra_args))
+
+    # run_test("Dev List", DevList(extra_args=extra_args))
+    run_test("Dev Audit", DevAudit(extra_args=extra_args))
     run_test("Dev Sync", DevSync(extra_args=extra_args))
     run_test("Dev Prune", DevPrune(extra_args=extra_args))
     print(f"Key down: {key_down_to_shared}")
