4cea1fc2363cb65f08e7a3b5af280cb60e7c331f
Jordan Mance <mancej@snagajob.com>
2020-07-21 21:08:36 -0400

    Tweaking git/add action retries

--- src/figcli/test/cli/actions/get.py
+++ src/figcli/test/cli/actions/get.py
@@ -14,23 +14,23 @@ class GetAction(FiggyAction):
     def get(self, key, value, get_more=False, expect_missing=False, no_decrypt=False, retry=True):
         response = self.expect(['.*PS Name.*', pexpect.TIMEOUT])
 
-        if response == 1:
+        if response == 1 and retry:
             self.alert(f"Received timeout, PERFORMING RETRY for KEY: {key}")
             self.get(key, value, get_more=get_more, expect_missing=expect_missing, retry=False)
-
-        self.sendline(key)
-        if expect_missing:
-            self.expect(f'.*Invalid PS Name specified.*')
-            print("Missing parameter validated.")
-        elif no_decrypt:
-            self.expect(f'.*do not have access.*')
-            print("Lack of decrypt permissions validated.")
         else:
-            self.expect(f'.*{value}.*Get another.*')
-            print(f"Expected value of {value} validated.")
-        if get_more:
-            print("Getting another")
-            self.sendline('y')
-        else:
-            print("Not getting another")
-            self.sendline('n')
+            self.sendline(key)
+            if expect_missing:
+                self.expect(f'.*Invalid PS Name specified.*')
+                print("Missing parameter validated.")
+            elif no_decrypt:
+                self.expect(f'.*do not have access.*')
+                print("Lack of decrypt permissions validated.")
+            else:
+                self.expect(f'.*{value}.*Get another.*')
+                print(f"Expected value of {value} validated.")
+            if get_more:
+                print("Getting another")
+                self.sendline('y')
+            else:
+                print("Not getting another")
+                self.sendline('n')
--- src/figcli/test/cli/actions/put.py
+++ src/figcli/test/cli/actions/put.py
@@ -14,24 +14,28 @@ class PutAction(FiggyAction):
                                        encoding='utf-8'),
                          extra_args=extra_args)
 
-    def add(self, key, value, desc, delete_first=True, add_more=False):
+    def add(self, key, value, desc, delete_first=True, add_more=False, retry=True):
         if delete_first:
             delete = DeleteAction(extra_args=self.extra_args)
             delete.delete(key)
 
-        self.expect('.*Please input a PS Name.*')
-        self.sendline(key)
-        self.expect('.*Please input a value.*')
-        self.sendline(value)
-        self.expect('.*Please input an optional.*')
-        self.sendline(desc)
-        self.expect('.*secret?.*')
-        self.sendline('n')
-        self.expect('.*another.*')
-        if add_more:
-            self.sendline('y')
+        result = self.expect(['.*Please input a PS Name.*', pexpect.TIMEOUT])
+        if result == 1 and retry:
+            self.alert(f"Executing retry for Put: {key}")
+            self.add(key, value, desc, delete_first=delete_first, add_more=add_more, retry=False)
         else:
+            self.sendline(key)
+            self.expect('.*Please input a value.*')
+            self.sendline(value)
+            self.expect('.*Please input an optional.*')
+            self.sendline(desc)
+            self.expect('.*secret?.*')
             self.sendline('n')
+            self.expect('.*another.*')
+            if add_more:
+                self.sendline('y')
+            else:
+                self.sendline('n')
 
     def add_encrypt_app(self, key, value, desc, add_more=False):
         delete = DeleteAction(extra_args=self.extra_args)
