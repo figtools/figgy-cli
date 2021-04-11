f824dd3b4361f061939094f7b54c5fda7359acde
Jordan Mance <mancej@snagajob.com>
2021-04-11 19:01:23 -0400

    Multi-threaded scan support

--- src/figcli/svcs/audit.py
+++ src/figcli/svcs/audit.py
@@ -1,4 +1,6 @@
 import logging
+import sys
+import time
 from typing import List
 
 from cachetools import TTLCache, cached
@@ -7,6 +9,7 @@ from figgy.data.dao.config import ConfigDao
 from figgy.models.audit_log import AuditLog
 
 from figcli.svcs.cache_manager import CacheManager
+from figcli.utils.utils import Utils
 
 log = logging.getLogger(__name__)
 
@@ -23,21 +26,22 @@ class AuditService:
                                 parameter_type: str = None,
                                 before: int = None,
                                 after: int = None) -> List[str]:
-
         logs: List[AuditLog] = self._audit.find_logs(filter=filter, parameter_type=parameter_type, before=before,
                                                      after=after)
 
         return [cfg.parameter_name for cfg in logs]
 
-    @cached(TTLCache(maxsize=5, ttl=30))
+    @cached(TTLCache(maxsize=10, ttl=500))
     def get_audit_logs_matching(self, filter: str = None,
                                 parameter_type: str = None,
                                 before: int = None,
                                 after: int = None) -> List[AuditLog]:
 
-        return self._audit.find_logs(filter=filter, parameter_type=parameter_type, before=before, after=after)
+        result = self._audit.find_logs_parallel(threads=5, filter=filter, parameter_type=parameter_type, before=before,
+                                                after=after)
+
+        return result
 
     @cached(TTLCache(maxsize=5, ttl=30))
     def get_parameter_logs(self, name: str) -> List[AuditLog]:
-
         return self._audit.get_audit_logs(ps_name=name)
