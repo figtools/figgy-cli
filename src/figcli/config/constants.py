4820916f3a37885e517e3c52b841704bda170236
Jordan Mance <jordanmance@gmail.com>
2022-12-09 14:00:42 -0600

    Mancej/cli tuning (#20)
    * Increasing audit max threads
    
    * Adding release notes
    
    Co-authored-by: Jordan Mance <mancej@snagajob.com>

--- src/figcli/config/constants.py
+++ src/figcli/config/constants.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 # If cloud has been updated with required changes, please updated REQUIRES_CLI_VERSION in figtools/figgy
-VERSION = '1.2.8'
+VERSION = '1.2.9'
 CLI_NAME = 'figgy'
 PROJECT_NAME = 'figgy'
 
